1bd8b0f898e4e48ae3a03e088b1c0661f265e521
Rodrigo <rdg.hora1@gmail.com>
2023-12-07 11:21:19 -0300

    Variaveis de ambiente .env.local

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
-import Home from "templates/Home";
+import { GetServerSideProps } from "next";
 
 export default function Index() {
-  return <Home />;
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <span>Oi</span>
+    </>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {},
+  };
+};
